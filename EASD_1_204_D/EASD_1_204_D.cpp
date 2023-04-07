2dadd460c264b95b863b593f64330d8fe8d9d327
falskuyyy <ahmdfalah10@gmail.com>
2023-04-07 10:20:24 +0700

    Add project files.

---
+++ EASD_1_204_D/EASD_1_204_D.cpp
@@ -0,0 +1,14 @@
+1.//sangat di butuhkan karna dari setiap masalah harus di analisa terlebih dahulu satu persatu supaya dapat menentukan penyelesaian yang sesuai dari masalah tersebut
+// 2.struktur data dibedakan menjadi dua yaitu 
+  //-statik (ukuran tetap,tidak bisa berubah) array digunakan saat struktur datanya sudah tertentu atau valid
+  //-dinamis (fleksibel bisa di tambah dan di kurang) belum di ketahui data nya
+// faktor faktor yang mempengaruhi efesiensi dalam suatu program
+// - kecepatan prosesor
+//-compiler yang bertugas sebagai menerjemahkan dari pmrograman menjadi bahasa komputer
+//- operatting system
+// - bahasa pemrograman
+// - ukuran sesuai input
+// 4.buble sort bagi saya adalah yang paling mudah dipahami dan sederhana cocok jika mengurut kan data yang sedikit
+// 5.-kuadratic :bublesort,selectionsort,insertionsort,shellsort
+  // -logliner  :mergesort,quicksort
+
