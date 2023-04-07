ee32f03bbadf3e2d4a55fd2ddc4b59b8f2eb2310
falskuyyy <ahmdfalah10@gmail.com>
2023-04-07 10:28:28 +0700

    jawaban nomer 1

--- EASD_1_204_D/EASD_1_204_D.cpp
+++ EASD_1_204_D/EASD_1_204_D.cpp
@@ -1,4 +1,4 @@
-1.//sangat di butuhkan karna dari setiap masalah harus di analisa terlebih dahulu satu persatu supaya dapat menentukan penyelesaian yang sesuai dari masalah tersebut
+1.//sangat di butuhkan karna dari setiap masalah harus di analisa terlebih dahulu satu persatu supaya dapat menentukan penyelesaian yang sesuai dari masalah tersebut 
 // 2.struktur data dibedakan menjadi dua yaitu 
   //-statik (ukuran tetap,tidak bisa berubah) array digunakan saat struktur datanya sudah tertentu atau valid
   //-dinamis (fleksibel bisa di tambah dan di kurang) belum di ketahui data nya 
