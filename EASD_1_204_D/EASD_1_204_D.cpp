6ab471bef0b2e10fe7d402ae6de10c53f7c48b44
falskuyyy <ahmdfalah10@gmail.com>
2023-04-07 10:26:48 +0700

    jawaban nomer 3

--- EASD_1_204_D/EASD_1_204_D.cpp
+++ EASD_1_204_D/EASD_1_204_D.cpp
@@ -2,12 +2,12 @@
 // 2.struktur data dibedakan menjadi dua yaitu 
   //-statik (ukuran tetap,tidak bisa berubah) array digunakan saat struktur datanya sudah tertentu atau valid
   //-dinamis (fleksibel bisa di tambah dan di kurang) belum di ketahui data nya
-// faktor faktor yang mempengaruhi efesiensi dalam suatu program
+// 3faktor faktor yang mempengaruhi efesiensi dalam suatu program
 // - kecepatan prosesor
 //-compiler yang bertugas sebagai menerjemahkan dari pmrograman menjadi bahasa komputer
 //- operatting system
 // - bahasa pemrograman
-// - ukuran sesuai input
+// - ukuran sesuai input 
 // 4.buble sort bagi saya adalah yang paling mudah dipahami dan sederhana cocok jika mengurut kan data yang sedikit 
 // 5.-kuadratic :bublesort,selectionsort,insertionsort,shellsort
   // -logliner  :mergesort,quicksort 
