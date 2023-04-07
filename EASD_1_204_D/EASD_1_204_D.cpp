f0b2c67028ec68f4861341331f9acec30e2f8df1
falskuyyy <ahmdfalah10@gmail.com>
2023-04-07 10:28:01 +0700

    jawaban nomer 2

--- EASD_1_204_D/EASD_1_204_D.cpp
+++ EASD_1_204_D/EASD_1_204_D.cpp
@@ -1,7 +1,7 @@
 1.//sangat di butuhkan karna dari setiap masalah harus di analisa terlebih dahulu satu persatu supaya dapat menentukan penyelesaian yang sesuai dari masalah tersebut
 // 2.struktur data dibedakan menjadi dua yaitu 
   //-statik (ukuran tetap,tidak bisa berubah) array digunakan saat struktur datanya sudah tertentu atau valid
-  //-dinamis (fleksibel bisa di tambah dan di kurang) belum di ketahui data nya
+  //-dinamis (fleksibel bisa di tambah dan di kurang) belum di ketahui data nya 
 // 3faktor faktor yang mempengaruhi efesiensi dalam suatu program
 // - kecepatan prosesor
 //-compiler yang bertugas sebagai menerjemahkan dari pmrograman menjadi bahasa komputer
